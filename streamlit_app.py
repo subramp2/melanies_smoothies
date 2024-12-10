3a8bd5d2d2389e28d5b0b9890371a5cf59d4d23e
subramp2 <subramp2@ccf.org>
2024-12-10 10:32:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ if ingredients_list:
         
         st.subheader(fruit_chosen + ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
-        fv_df= st.dataframe(data=smoothiefroot_response.json(), use_container_width= True)
+        fv_df= st.dataframe(data=fruityvice_response.json(), use_container_width= True)
 
     # st.write(ingredients_string)    
 
